de39a09487688cd0c4bf2efd0b8a44f6f7f4a696
LablancaDev <valenciano_david@hotmail.com>
2024-09-03 23:38:25 +0200

    update styles

--- src/componentes/Layout.tsx
+++ src/componentes/Layout.tsx
@@ -1,8 +1,6 @@
 // src/components/Layout.tsx
 import React from 'react';
-import NavBar from './NavBar';
 import Footer from './Footer';
-import { NavBarProps } from './NavBar';
 
 // Define la interfaz para las props del Layout
 interface LayoutProps {
